0dbe0a6fced8757747ba6d902f4f72c853f59344
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-10-21 08:47:54 -0300

    cadastro e mensagem de erro

--- pages/Cadastro/Cadastro.php
+++ pages/Cadastro/Cadastro.php
@@ -25,31 +25,33 @@ if (isset($_COOKIE['acesso']) && $_COOKIE['acesso'] != 'administrador') {
         <form action="CriaCadastro/CriaCadastro.php" method="post" class="formulario">
             <div class="inputBox">
                 <label for="login" class="formulario__label">User:</label>
-                <input required name="user" placeholder="Digite o CPF" type="text" id="login" class="formulario__input">
+                <input name="user" placeholder="Digite o CPF" type="text" id="login" class="formulario__input">
             </div>
             <div class="inputBox">
                 <label for="senha" class="formulario__label">Senha:</label>
-                <input required name="senha" placeholder="Digite a senha" type="text" id="senha" class="formulario__input">
+                <input  name="senha" placeholder="Digite a senha" type="text" id="senha" class="formulario__input">
             </div>
             <div class="inputBox">
                 <label for="acesso" class="formulario__label">Acesso:</label>
-                <select required name="acesso" id="acesso" class="formulario__input">
+                <select name="acesso" id="acesso" class="formulario__input">
                     <option value="administrador">Administrador</option>
                     <option value="funcionario">Funcionario </option>
                     <option value="aluno">Aluno</option>
                 </select>
             </div>
-            <? if (isset($_SESSION['cadastro'])) { 
-               if($_SESSION['cadastro'] == 'sucesso'){ ?>
-                  <span class="formulario__mensagemErro">Usuário cadastrado com sucesso!</span>     
-              <? } ?>
-              <? if($_SESSION['cadastro'] == 'erro1'){ ?>
-                  <span class="formulario__mensagemErro">Usuário ja cadastrado!</span>     
-              <? } ?>
-              <? if($_SESSION['cadastro'] == 'erro2'){ ?>
-                  <span class="formulario__mensagemErro">Informe usuario, senha e selecione o tipo de acesso!</span>     
-              <? } ?>
-            ?>
+            <?php
+            session_start();
+            if (isset($_SESSION['cadastro'])) { ?>
+                <?php if ($_SESSION['cadastro'] == 'sucesso') { ?>
+                    <span class="formulario__mensagemErro">Usuário cadastrado com sucesso!</span>
+                <?php } ?>
+                <?php if ($_SESSION['cadastro'] == 'erro1') { ?>
+                    <span class="formulario__mensagemErro">Usuário ja cadastrado!</span>
+                <?php } ?>
+                <?php if ($_SESSION['cadastro'] == 'erro2') { ?>
+                    <span class="formulario__mensagemErro">Informe usuario, senha e selecione o tipo de acesso!</span>
+                <?php } ?>
+            <?php } ?>
             <button class="formulario__button" type="submit">Cadastrar</button>
         </form>
     </main>
